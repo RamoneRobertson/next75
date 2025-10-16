6a5e71bd90477be044ef9c323d90c7f8bad2c8fd
Ramone Robertson <ramonerobertson10@gmail.com>
2025-10-16 12:59:24 +0900

    initial commit

---
+++ src/main/java/io/next75/Next75Application.java
@@ -0,0 +1,13 @@
+package io.next75;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Next75Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Next75Application.class, args);
+  }
+
+}
---
+++ src/test/java/io/next75/Next75ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.next75;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Next75ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
