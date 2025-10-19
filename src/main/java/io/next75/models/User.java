68b7f595b5ac2219c4464175950a6ac406d518db
Ramone Robertson <ramonerobertson10@gmail.com>
2025-10-19 22:47:42 +0900

    edits to data sql file also setup postgres db

--- src/main/java/io/next75/models/User.java
+++ src/main/java/io/next75/models/User.java
@@ -1,15 +1,35 @@
 package io.next75.models;
 
 import io.next75.enums.Gender;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
 
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
+@Getter
+@Setter
+@Table(name = "users")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   private String name;
+  @Email
+  private String email;
   private Gender gender;
   private BigDecimal currentWeight;
   private BigDecimal targetWeight;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- Configure enums
-CREATE TYPE goal_type AS ENUM ('LOSS', 'GAIN', 'MAINTAIN');
-CREATE TYPE gender_type AS ENUM('MALE', 'FEMALE', 'NONE');
+-- CREATE TYPE goal_type AS ENUM ('LOSS', 'GAIN', 'MAINTAIN');
+-- CREATE TYPE gender_type AS ENUM('MALE', 'FEMALE', 'NONE');
 
 -- Ensure the 'users' table exists
 CREATE TABLE IF NOT EXISTS users (
@@ -16,8 +16,8 @@ CREATE TABLE IF NOT EXISTS users (
     target_weight   DECIMAL(5, 2)                             NOT NULL,
     height          DECIMAL(5, 2)                             NOT NULL,
     goal            goal_type                                 NOT NULL,
-    created_at      TIMESTAMP       DEFAULT CURRENT_TIMESTAMP NOT NULL ,
-    updated_at      TIMESTAMP       DEFAULT CURRENT_TIMESTAMP NOT NULL
+    created_at TIMESTAMP       DEFAULT CURRENT_TIMESTAMP NOT NULL ,
+    updated_at  TIMESTAMP       DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 -- Create Lists table
@@ -46,11 +46,21 @@ CREATE TABLE IF NOT EXISTS tasks(
         FOREIGN KEY (list_id) REFERENCES lists (id)
 );
 
+-- Create Meals Table
+CREATE TABLE IF NOT EXISTS meals(
+    id BIGINT PRIMARY KEY,
+    user_id UUID NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    calories DECIMAL(5, 2),
+    created_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT  CURRENT_TIMESTAMP NOT NULL
+
+);
 
 -- Insert User Records
 
 -- USER 1
-INSERT INTO users (id, name, email, password, gender, birthdate, calorie_limit, streak, current_weight, target_weight, height, goal, created_at, updated_at)
+INSERT INTO users (id, name, email, password, gender, birthdate, calorie_limit, streak, current_weight, target_weight, height, goal)
 SELECT '123e4567-e89b-12d3-a456-426614174001',
        'John Doe',
        'john.doe@example.com',
@@ -62,17 +72,12 @@ SELECT '123e4567-e89b-12d3-a456-426614174001',
        93,
        84,
        183,
-       'LOSS',
-       '2025-10-19',
-       '2025-10-19'
+       'LOSS'
 
-WHERE NOT EXISTS (SELECT 1
-                  FROM users
-                  WHERE id = '123e4567-e89b-12d3-a456-426614174001');
-
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = '123e4567-e89b-12d3-a456-426614174001');
 
 -- USER 2
-INSERT INTO users (id, name, email, password, gender, birthdate, calorie_limit, streak, current_weight, target_weight, height, goal, created_at, updated_at)
+INSERT INTO users (id, name, email, password, gender, birthdate, calorie_limit, streak, current_weight, target_weight, height, goal)
 SELECT '223e4567-e89b-12d3-a456-426614174002',
        'Jane Smith',
        'jane.smith@example.com',
@@ -84,14 +89,11 @@ SELECT '223e4567-e89b-12d3-a456-426614174002',
        65,
        60,
        170,
-       'LOSS',
-       '2025-10-19',
-       '2025-10-19'
+       'LOSS'
 WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = '223e4567-e89b-12d3-a456-426614174002');
 
-
 -- User 3
-INSERT INTO users (id, name, email, password, gender, birthdate, calorie_limit, streak, current_weight, target_weight, height, goal, created_at, updated_at)
+INSERT INTO users (id, name, email, password, gender, birthdate, calorie_limit, streak, current_weight, target_weight, height, goal)
 SELECT '323e4567-e89b-12d3-a456-426614174003',
        'Michael Johnson',
        'michael.johnson@example.com',
@@ -103,14 +105,11 @@ SELECT '323e4567-e89b-12d3-a456-426614174003',
        82,
        85,
        178,
-       'GAIN',
-       '2025-10-19',
-       '2025-10-19'
+       'GAIN'
 WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = '323e4567-e89b-12d3-a456-426614174003');
 
-
 -- User 4
-INSERT INTO users (id, name, email, password, gender, birthdate, calorie_limit, streak, current_weight, target_weight, height, goal, created_at, updated_at)
+INSERT INTO users (id, name, email, password, gender, birthdate, calorie_limit, streak, current_weight, target_weight, height, goal)
 SELECT '423e4567-e89b-12d3-a456-426614174004',
        'Emily Davis',
        'emily.davis@example.com',
@@ -122,14 +121,11 @@ SELECT '423e4567-e89b-12d3-a456-426614174004',
        70,
        68,
        165,
-       'MAINTAIN',
-       '2025-10-19',
-       '2025-10-19'
+       'MAINTAIN'
 WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = '423e4567-e89b-12d3-a456-426614174004');
 
-
 -- User 5
-INSERT INTO users (id, name, email, password, gender, birthdate, calorie_limit, streak, current_weight, target_weight, height, goal, created_at, updated_at)
+INSERT INTO users (id, name, email, password, gender, birthdate, calorie_limit, streak, current_weight, target_weight, height, goal)
 SELECT '523e4567-e89b-12d3-a456-426614174005',
        'Chris Lee',
        'chris.lee@example.com',
@@ -141,9 +137,219 @@ SELECT '523e4567-e89b-12d3-a456-426614174005',
        75,
        70,
        172,
-       'LOSS',
-       '2025-10-19',
-       '2025-10-19'
+       'LOSS'
 WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = '523e4567-e89b-12d3-a456-426614174005');
 
+-- Creating Lists for users
 
+-- LIST 1
+INSERT INTO lists (id, user_id, start_date)
+SELECT '123e4567-e89b-12d3-a456-426614174101',
+       '123e4567-e89b-12d3-a456-426614174001',
+       current_date
+
+WHERE NOT EXISTS(SELECT 1 FROM lists WHERE id = '123e4567-e89b-12d3-a456-426614174101');
+
+
+-- LIST 2
+INSERT INTO lists (id, user_id, start_date )
+SELECT '123e4567-e89b-12d3-a456-426614174102',
+       '223e4567-e89b-12d3-a456-426614174002',
+       current_date
+
+WHERE NOT EXISTS(SELECT 1 FROM lists WHERE id = '123e4567-e89b-12d3-a456-426614174102');
+
+-- LIST 3
+INSERT INTO lists (id, user_id, start_date )
+SELECT '123e4567-e89b-12d3-a456-426614174103',
+       '323e4567-e89b-12d3-a456-426614174003',
+       current_date
+
+WHERE NOT EXISTS(SELECT 1 FROM lists WHERE id = '123e4567-e89b-12d3-a456-426614174103');
+
+-- LIST 4
+INSERT INTO lists (id, user_id, start_date )
+SELECT '123e4567-e89b-12d3-a456-426614174104',
+       '423e4567-e89b-12d3-a456-426614174004',
+       current_date
+
+WHERE NOT EXISTS(SELECT 1 FROM lists WHERE id = '123e4567-e89b-12d3-a456-426614174104');
+
+
+-- LIST 5
+INSERT INTO lists (id, user_id, start_date )
+SELECT '123e4567-e89b-12d3-a456-426614174105',
+       '523e4567-e89b-12d3-a456-426614174005',
+       current_date
+
+WHERE NOT EXISTS(SELECT 1 FROM lists WHERE id = '123e4567-e89b-12d3-a456-426614174105');
+
+-- Creating tasks for lists
+
+-- LIST 1 Tasks
+INSERT INTO tasks(id, list_id, title)
+SELECT 1,
+       '123e4567-e89b-12d3-a456-426614174101',
+       'Outside Workout'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 1);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 2,
+       '123e4567-e89b-12d3-a456-426614174101',
+       'Inside Workout'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 2);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 3,
+       '123e4567-e89b-12d3-a456-426614174101',
+       'Drink a gallon of water'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 3);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 4,
+       '123e4567-e89b-12d3-a456-426614174101',
+       'Read 10 pages of a non-fiction book'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 4);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 5,
+       '123e4567-e89b-12d3-a456-426614174101',
+       'Take progress photo'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 5);
+
+-- LIST 2 Tasks
+INSERT INTO tasks(id, list_id, title)
+SELECT 6,
+       '123e4567-e89b-12d3-a456-426614174102',
+       'Outside Workout'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 6);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 7,
+       '123e4567-e89b-12d3-a456-426614174102',
+       'Inside Workout'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 7);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 8,
+       '123e4567-e89b-12d3-a456-426614174102',
+       'Drink a gallon of water'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 8);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 9,
+       '123e4567-e89b-12d3-a456-426614174102',
+       'Read 10 pages of a non-fiction book'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 9);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 10,
+       '123e4567-e89b-12d3-a456-426614174102',
+       'Take progress photo'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 10);
+
+-- LIST 3 Tasks
+INSERT INTO tasks(id, list_id, title)
+SELECT 11,
+       '123e4567-e89b-12d3-a456-426614174103',
+       'Outside Workout'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 11);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 12,
+       '123e4567-e89b-12d3-a456-426614174103',
+       'Inside Workout'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 12);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 13,
+       '123e4567-e89b-12d3-a456-426614174103',
+       'Drink a gallon of water'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 13);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 14,
+       '123e4567-e89b-12d3-a456-426614174103',
+       'Read 10 pages of a non-fiction book'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 14);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 15,
+       '123e4567-e89b-12d3-a456-426614174103',
+       'Take progress photo'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 15);
+
+-- LIST 4 Tasks
+INSERT INTO tasks(id, list_id, title)
+SELECT 16,
+       '123e4567-e89b-12d3-a456-426614174104',
+       'Outside Workout'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 16);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 17,
+       '123e4567-e89b-12d3-a456-426614174104',
+       'Inside Workout'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 17);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 18,
+       '123e4567-e89b-12d3-a456-426614174104',
+       'Drink a gallon of water'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 18);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 19,
+       '123e4567-e89b-12d3-a456-426614174104',
+       'Read 10 pages of a non-fiction book'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 19);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 20,
+       '123e4567-e89b-12d3-a456-426614174104',
+       'Take progress photo'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 20);
+
+-- LIST 5 Tasks
+INSERT INTO tasks(id, list_id, title)
+SELECT 21,
+       '123e4567-e89b-12d3-a456-426614174105',
+       'Outside Workout'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 21);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 22,
+       '123e4567-e89b-12d3-a456-426614174105',
+       'Inside Workout'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 22);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 23,
+       '123e4567-e89b-12d3-a456-426614174105',
+       'Drink a gallon of water'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 23);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 24,
+       '123e4567-e89b-12d3-a456-426614174105',
+       'Read 10 pages of a non-fiction book'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 24);
+
+INSERT INTO tasks(id, list_id, title)
+SELECT 25,
+       '123e4567-e89b-12d3-a456-426614174105',
+       'Take progress photo'
+WHERE NOT EXISTS(SELECT 1 FROM tasks WHERE id = 25);
+
+-- CREATE TABLE IF NOT EXISTS tasks(
+--                                     id BIGINT PRIMARY KEY,
+--                                     list_id UUID NOT NULL ,
+--                                     title VARCHAR(255) NOT NULL,
+--                                     description TEXT NULL,
+--                                     is_completed BOOLEAN DEFAULT false,
+--                                     completed_at TIMESTAMP NULL,
+--                                           TIMESTAMP       DEFAULT CURRENT_TIMESTAMP NOT NULL,
+--                                           TIMESTAMP       DEFAULT CURRENT_TIMESTAMP NOT NULL,
+--                                     CONSTRAINT  tasks_lists_id_fk
+--                                         FOREIGN KEY (list_id) REFERENCES lists (id)
+-- );
